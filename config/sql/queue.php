2e1df6e946edcec1b30814718859e78e92369890
MGriesbach <MGriesbach@gmail.com>
2009-06-08 15:32:42 +0200

    Added Readme/Installation instructions.  Corrected schema file for installation.

---
+++ config/sql/queue.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @author MGriesbach@gmail.com
+ * @package QueuePlugin
+ * @subpackage QueuePlugin.Models
+ */
+class queueSchema extends CakeSchema {
+	var $name = 'queue';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	public $queued_tasks = array(
+		'id' => array(
+			'type' => 'integer',
+			'null' => false,
+			'default' => NULL,
+			'length' => 10,
+			'key' => 'primary'
+		),
+		'jobtype' => array(
+			'type' => 'string',
+			'null' => false,
+			'length' => 45
+		),
+		'data' => array(
+			'type' => 'text',
+			'null' => true,
+			'default' => NULL
+		),
+		'created' => array(
+			'type' => 'datetime',
+			'null' => false
+		),
+		'fetched' => array(
+			'type' => 'datetime',
+			'null' => true,
+			'default' => NULL
+		),
+		'completed' => array(
+			'type' => 'datetime',
+			'null' => true,
+			'default' => NULL
+		),
+		'failed' => array(
+			'type' => 'integer',
+			'null' => false,
+			'default' => '0',
+			'length' => 3
+		),
+		'indexes' => array(
+			'PRIMARY' => array(
+				'column' => 'id',
+				'unique' => 1
+			)
+		)
+	);
+}
+?>
