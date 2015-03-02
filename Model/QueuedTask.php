5907258da852959b0e1a19fd87c5276addc835a2
Corie Slate <corie@houseoftech.com>
2015-03-02 10:59:13 +0100

    Include progress field in _findProgress() method

--- Model/QueuedTask.php
+++ Model/QueuedTask.php
@@ -355,6 +355,7 @@ class QueuedTask extends QueueAppModel {
 			$query['fields'] = [
 				$this->alias . '.reference',
 				'(CASE WHEN ' . $this->alias . '.notbefore > NOW() THEN \'NOT_READY\' WHEN ' . $this->alias . '.fetched IS null THEN \'NOT_STARTED\' WHEN ' . $this->alias . '.fetched IS NOT null AND ' . $this->alias . '.completed IS null AND ' . $this->alias . '.failed = 0 THEN \'IN_PROGRESS\' WHEN ' . $this->alias . '.fetched IS NOT null AND ' . $this->alias . '.completed IS null AND ' . $this->alias . '.failed > 0 THEN \'FAILED\' WHEN ' . $this->alias . '.fetched IS NOT null AND ' . $this->alias . '.completed IS NOT null THEN \'COMPLETED\' ELSE \'UNKNOWN\' END) AS status',
+				$this->alias . '.progress',
 				$this->alias . '.failure_message'
 			];
 			if (isset($query['conditions']['exclude'])) {
@@ -380,6 +381,9 @@ class QueuedTask extends QueueAppModel {
 				'reference' => $result[$this->alias]['reference'],
 				'status' => $result[0]['status']
 			];
+			if (!empty($result[$this->alias]['progress'])) {
+				$results[$k]['progress'] = $result[$this->alias]['progress'];
+			}
 			if (!empty($result[$this->alias]['failure_message'])) {
 				$results[$k]['failure_message'] = $result[$this->alias]['failure_message'];
 			}
