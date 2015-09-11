8e62978215bf08ee956d03d41a0123b4271b9c61
Mikkel Bonde <mikbonde@gmail.com>
2015-09-11 15:28:00 +0200

    Improvements for it to be working on MySQL that is not MariaDB fork

---
+++ config/Migrations/20150911132343_improvements_for_mysql.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ImprovementsForMysql extends AbstractMigration {
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change() {
+        $table = $this->table('queued_tasks');
+
+        $table->changeColumn('id', 'integer', [
+            'length' => 11,
+            'null' => false,
+            'default' => null,
+        ]);
+
+        $table->changeColumn('jobtype', 'string', [
+            'length' => 45,
+            'null' => false,
+            'default' => null,
+        ]);
+
+        $table->changeColumn('status', 'string', [
+           'length' => 255,
+            'null' => false,
+            'default' => null,
+        ]);
+    }
+}
