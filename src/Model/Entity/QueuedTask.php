1fd13d927b9b2cf2251f03433184009f37f20a08
Mark Scherer <euromark@web.de>
2015-12-31 09:55:20 +0100

    remove alias

--- src/Model/Entity/QueuedTask.php
+++ src/Model/Entity/QueuedTask.php
@@ -7,7 +7,7 @@ class QueuedTask extends Entity {
 
 	/*
 	protected function _getStatus() {
-		// $this->virtualFields['status'] = '(CASE WHEN ' . $this->alias . '.notbefore > NOW() THEN \'NOT_READY\' WHEN ' . $this->alias . '.fetched IS null THEN \'NOT_STARTED\' WHEN ' . $this->alias . '.fetched IS NOT null AND ' . $this->alias . '.completed IS null AND ' . $this->alias . '.failed = 0 THEN \'IN_PROGRESS\' WHEN ' . $this->alias . '.fetched IS NOT null AND ' . $this->alias . '.completed IS null AND ' . $this->alias . '.failed > 0 THEN \'FAILED\' WHEN ' . $this->alias . '.fetched IS NOT null AND ' . $this->alias . '.completed IS NOT null THEN \'COMPLETED\' ELSE \'UNKNOWN\' END)';
+		// $this->virtualFields['status'] = '(CASE WHEN ' . 'notbefore > NOW() THEN \'NOT_READY\' WHEN ' . 'fetched IS null THEN \'NOT_STARTED\' WHEN ' . 'fetched IS NOT null AND ' . 'completed IS null AND ' . 'failed = 0 THEN \'IN_PROGRESS\' WHEN ' . 'fetched IS NOT null AND ' . 'completed IS null AND ' . 'failed > 0 THEN \'FAILED\' WHEN ' . 'fetched IS NOT null AND ' . 'completed IS NOT null THEN \'COMPLETED\' ELSE \'UNKNOWN\' END)';
 	}
 	*/
 
