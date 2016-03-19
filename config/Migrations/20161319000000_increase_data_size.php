e5294fc27f52de84aa035416ec06f71ce3207dc8
Mark Scherer <euromark@web.de>
2016-03-19 12:20:40 +0100

    Fix CS

--- config/Migrations/20161319000000_increase_data_size.php
+++ config/Migrations/20161319000000_increase_data_size.php
@@ -1,24 +1,26 @@
-
 <?php
 
 use Phinx\Migration\AbstractMigration;
 
 class IncreaseDataSize extends AbstractMigration {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     */
-    public function change() {
-        $table = $this->table('queued_tasks');
 
-        $table->changeColumn('data', 'text', [
-            'length' => 4294967295,
-            'null' => true,
-            'default' => null,
-        ]);
-    }
+	/**
+	 * Change Method.
+	 *
+	 * Write your reversible migrations using this method.
+	 *
+	 * More information on writing migrations is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+	 * @return void
+	 */
+	public function change() {
+		$table = $this->table('queued_tasks');
+
+		$table->changeColumn('data', 'text', [
+			'length' => 4294967295,
+			'null' => true,
+			'default' => null,
+		]);
+	}
+
 }
