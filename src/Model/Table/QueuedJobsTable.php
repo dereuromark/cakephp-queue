79299502441ad1716ea6ee2caf2bad83a0c763d1
st.meyer <st.meyer@technisat.de>
2017-10-18 16:04:20 +0200

    add phpdoc

--- src/Model/Table/QueuedJobsTable.php
+++ src/Model/Table/QueuedJobsTable.php
@@ -588,6 +588,10 @@ class QueuedJobsTable extends Table {
 		}
 	}
 
+    /**
+     * get the name of the driver
+     * @return string
+     */
     protected function _getDriverName()
     {
         $className = explode("\\", $this->connection()->config()['driver']);
