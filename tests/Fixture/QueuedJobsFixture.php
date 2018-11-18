19b2e05c59db16e62e747e29ef85453b6af4aabf
mscherer <mark.scherer@spryker.com>
2018-11-19 00:00:56 +0100

    Update fixture.

--- tests/Fixture/QueuedJobsFixture.php
+++ tests/Fixture/QueuedJobsFixture.php
@@ -9,41 +9,65 @@ namespace Queue\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
-/**
- */
 class QueuedJobsFixture extends TestFixture {
-
-	/**
-	 * Records
-	 *
-	 * @var array
-	 */
-	public $records = [];
 
 	/**
 	 * Fields
 	 *
 	 * @var array
 	 */
-	public $fields = [
-		'id' => ['type' => 'integer'],
-		'job_type' => ['type' => 'string', 'length' => 45, 'null' => false, 'default' => null],
-		'data' => ['type' => 'text', 'null' => true, 'default' => null],
-		'priority' => ['type' => 'integer', 'length' => 3, 'null' => false, 'default' => 5],
-		'job_group' => ['type' => 'string', 'length' => 255, 'null' => true],
-		'reference' => ['type' => 'string', 'length' => 255, 'null' => true],
-		'created' => ['type' => 'datetime', 'null' => false],
-		'notbefore' => ['type' => 'datetime', 'null' => true],
-		'fetched' => ['type' => 'datetime', 'null' => true],
-		'completed' => ['type' => 'datetime', 'null' => true],
-		'progress' => ['type' => 'float', 'null' => true],
-		'status' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null],
-		'failed' => ['type' => 'integer', 'length' => 3, 'null' => false, 'default' => 0],
-		'failure_message' => ['type' => 'text', 'null' => true, 'default' => null],
-		'workerkey' => ['type' => 'string', 'length' => 45, 'null' => true, 'default' => null],
-		'_constraints' => [
-			'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
-		],
-	];
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'job_type' => ['type' => 'string', 'length' => 45, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'data' => ['type' => 'text', 'length' => 16777215, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null],
+        'job_group' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'reference' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'notbefore' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'fetched' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'completed' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'progress' => ['type' => 'float', 'length' => null, 'precision' => null, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => ''],
+        'failed' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'failure_message' => ['type' => 'text', 'length' => 16777215, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null],
+        'workerkey' => ['type' => 'string', 'length' => 45, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'status' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8mb4_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'priority' => ['type' => 'integer', 'length' => 3, 'unsigned' => false, 'null' => false, 'default' => '5', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+	/**
+	 * Init method
+	 *
+	 * @return void
+	 */
+	public function init() {
+		$this->records = [
+			[
+				'id' => 1,
+				'job_type' => 'Lorem ipsum dolor sit amet',
+				'data' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+				'job_group' => 'Lorem ipsum dolor sit amet',
+				'reference' => 'Lorem ipsum dolor sit amet',
+				'created' => '2018-11-18 22:54:49',
+				'notbefore' => '2018-11-18 22:54:49',
+				'fetched' => '2018-11-18 22:54:49',
+				'completed' => '2018-11-18 22:54:49',
+				'progress' => 1,
+				'failed' => 1,
+				'failure_message' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+				'workerkey' => 'Lorem ipsum dolor sit amet',
+				'status' => 'Lorem ipsum dolor sit amet',
+				'priority' => 1
+			],
+		];
+		parent::init();
+	}
 
 }
