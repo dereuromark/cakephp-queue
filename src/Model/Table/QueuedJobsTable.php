4630440271778863d5ff3fa9a07e16d2148de254
mstroink <marijnstroink@gmail.com>
2019-07-16 09:42:06 +0200

    fix postgres error undefined function unix_timestamp on getStats()

--- src/Model/Table/QueuedJobsTable.php
+++ src/Model/Table/QueuedJobsTable.php
@@ -247,6 +247,11 @@ class QueuedJobsTable extends Table {
 						$runtime = $query->func()->avg("DATEDIFF(s, '1970-01-01 00:00:00', completed) - DATEDIFF(s, '1970-01-01 00:00:00', fetched)");
 						$fetchdelay = $query->func()->avg("DATEDIFF(s, '1970-01-01 00:00:00', fetched) - (CASE WHEN notbefore IS NULL THEN DATEDIFF(s, '1970-01-01 00:00:00', created) ELSE DATEDIFF(s, '1970-01-01 00:00:00', notbefore) END)");
 						break;
+					case static::DRIVER_POSTGRES:
+						$alltime = $query->func()->avg('EXTRACT(EPOCH FROM completed) - EXTRACT(EPOCH FROM created)');
+						$runtime = $query->func()->avg('EXTRACT(EPOCH FROM completed) - EXTRACT(EPOCH FROM fetched)');
+						$fetchdelay = $query->func()->avg('EXTRACT(EPOCH FROM fetched) - CASE WHEN notbefore IS NULL then EXTRACT(EPOCH FROM created) ELSE EXTRACT(EPOCH FROM notbefore) END');
+						break;
 				}
 					/**
 						 * @var \Cake\ORM\Query
