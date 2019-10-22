ca024267a6c21c0dfd0740ce466185b86bb233d0
mscherer <mark.scherer@spryker.com>
2019-10-23 01:48:57 +0200

    Adjust test case

--- tests/TestCase/Generator/Task/QueuedJobGeneratorTest.php
+++ tests/TestCase/Generator/Task/QueuedJobGeneratorTest.php
@@ -26,22 +26,27 @@ class QueuedJobGeneratorTest extends TestCase {
 	public function testCollect() {
 		$result = $this->task->collect();
 
+		$this->assertCount(1, $result);
+
+		/** @var \IdeHelper\Generator\Directive\Override $directive */
+		$directive = array_shift($result);
+		$this->assertSame('\Queue\Model\Table\QueuedJobsTable::createJob(0)', $directive->toArray()['method']);
+
+		$map = $directive->toArray()['map'];
 		$expected = [
-			'\Queue\Model\Table\QueuedJobsTable::createJob(0)' => [
-				'CostsExample' => '\Queue\Shell\Task\QueueCostsExampleTask::class',
-				'Email' => '\Queue\Shell\Task\QueueEmailTask::class',
-				'Example' => '\Queue\Shell\Task\QueueExampleTask::class',
-				'ExceptionExample' => '\Queue\Shell\Task\QueueExceptionExampleTask::class',
-				'Execute' => '\Queue\Shell\Task\QueueExecuteTask::class',
-				'Foo' => '\App\Shell\Task\QueueFooTask::class',
-				'MonitorExample' => '\Queue\Shell\Task\QueueMonitorExampleTask::class',
-				'ProgressExample' => '\Queue\Shell\Task\QueueProgressExampleTask::class',
-				'RetryExample' => '\Queue\Shell\Task\QueueRetryExampleTask::class',
-				'SuperExample' => '\Queue\Shell\Task\QueueSuperExampleTask::class',
-				'UniqueExample' => '\Queue\Shell\Task\QueueUniqueExampleTask::class',
-			],
+			'CostsExample' => '\Queue\Shell\Task\QueueCostsExampleTask::class',
+			'Email' => '\Queue\Shell\Task\QueueEmailTask::class',
+			'Example' => '\Queue\Shell\Task\QueueExampleTask::class',
+			'ExceptionExample' => '\Queue\Shell\Task\QueueExceptionExampleTask::class',
+			'Execute' => '\Queue\Shell\Task\QueueExecuteTask::class',
+			'Foo' => '\App\Shell\Task\QueueFooTask::class',
+			'MonitorExample' => '\Queue\Shell\Task\QueueMonitorExampleTask::class',
+			'ProgressExample' => '\Queue\Shell\Task\QueueProgressExampleTask::class',
+			'RetryExample' => '\Queue\Shell\Task\QueueRetryExampleTask::class',
+			'SuperExample' => '\Queue\Shell\Task\QueueSuperExampleTask::class',
+			'UniqueExample' => '\Queue\Shell\Task\QueueUniqueExampleTask::class',
 		];
-		$this->assertSame($expected, $result);
+		$this->assertSame($expected, $map);
 	}
 
 }
