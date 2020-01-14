4bc7d3737c3dc363c2845c698f2563afcc9520d2
mscherer <mark.scherer@spryker.com>
2020-01-14 16:52:14 +0100

    Fix CS

--- tests/test_app/src/Application.php
+++ tests/test_app/src/Application.php
@@ -9,13 +9,13 @@ use Cake\Routing\Middleware\RoutingMiddleware;
 class Application extends BaseApplication {
 
 	/**
-	 * @param \Cake\Http\MiddlewareQueue $middleware The middleware queue to set in your App Class
+	 * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to set in your App Class
 	 * @return \Cake\Http\MiddlewareQueue
 	 */
-	public function middleware(MiddlewareQueue $middleware): MiddlewareQueue {
-		$middleware->add(new RoutingMiddleware($this));
+	public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue {
+		$middlewareQueue->add(new RoutingMiddleware($this));
 
-		return $middleware;
+		return $middlewareQueue;
 	}
 
 	/**
