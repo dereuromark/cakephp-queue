b993fed2a3da64da5a6b148a67aa4fac4048bb62
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-06-30 18:43:08 +0200

    fix cs warning

--- src/Model/Entity/QueueProcess.php
+++ src/Model/Entity/QueueProcess.php
@@ -14,7 +14,7 @@ use Cake\ORM\Entity;
  * @property bool $terminate
  * @property string|null $server
  * @property string $workerkey
- * @property QueuedJob active_job
+ * @property \Queue\Model\Entity\QueuedJob active_job
  */
 class QueueProcess extends Entity {
 
