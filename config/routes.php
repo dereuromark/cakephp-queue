93190d8880a85b8aa8f8b8ec6deaa5ae11f6d323
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-09-14 18:21:48 +0200

    update routes.php to use RouteBuilder

--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,18 @@
 <?php
 
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 
-Router::prefix('Admin', function (RouteBuilder $routes) {
-	$routes->plugin('Queue', function (RouteBuilder $routes) {
-		$routes->connect('/', ['controller' => 'Queue', 'action' => 'index']);
+return static function (RouteBuilder $outer_routes) {
+	$outer_routes->prefix('Admin', function (RouteBuilder $routes) {
+		$routes->plugin('Queue', function (RouteBuilder $routes) {
+			$routes->connect('/', ['controller' => 'Queue', 'action' => 'index']);
 
-		$routes->fallbacks();
+			$routes->fallbacks();
+		});
 	});
-});
 
-Router::plugin('Queue', ['path' => '/queue'], function (RouteBuilder $routes) {
-	$routes->connect('/:controller');
-});
+	$outer_routes->plugin('Queue', ['path' => '/queue'], function (RouteBuilder $routes) {
+		$routes->connect('/:controller');
+	});
+};
+
