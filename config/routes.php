8a50b69af6d6779feff2e044a2ccfbf319335c66
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-09-14 18:29:06 +0200

    rename outer_routes to routes

--- config/routes.php
+++ config/routes.php
@@ -2,8 +2,8 @@
 
 use Cake\Routing\RouteBuilder;
 
-return static function (RouteBuilder $outer_routes) {
-	$outer_routes->prefix('Admin', function (RouteBuilder $routes) {
+return static function (RouteBuilder $routes) {
+	$routes->prefix('Admin', function (RouteBuilder $routes) {
 		$routes->plugin('Queue', function (RouteBuilder $routes) {
 			$routes->connect('/', ['controller' => 'Queue', 'action' => 'index']);
 
@@ -11,7 +11,7 @@ return static function (RouteBuilder $outer_routes) {
 		});
 	});
 
-	$outer_routes->plugin('Queue', ['path' => '/queue'], function (RouteBuilder $routes) {
+	$routes->plugin('Queue', [ 'path' => '/queue'], function (RouteBuilder $routes) {
 		$routes->connect('/:controller');
 	});
 };
