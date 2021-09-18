9d46a96aa2698b25130d1f1bdb665dcc5e0b3a65
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-09-18 18:03:37 +0200

    fix routes.php deprecations with cake4.3

--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,17 @@
 <?php
 
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 
-Router::prefix('Admin', function (RouteBuilder $routes) {
-	$routes->plugin('Queue', function (RouteBuilder $routes) {
-		$routes->connect('/', ['controller' => 'Queue', 'action' => 'index']);
+return function (RouteBuilder $routes) {
+	$routes->prefix('Admin', function (RouteBuilder $routes) {
+		$routes->plugin('Queue', function (RouteBuilder $routes) {
+			$routes->connect('/', ['controller' => 'Queue', 'action' => 'index']);
 
-		$routes->fallbacks();
+			$routes->fallbacks();
+		});
 	});
-});
 
-Router::plugin('Queue', ['path' => '/queue'], function (RouteBuilder $routes) {
-	$routes->connect('/:controller');
-});
+	$routes->plugin('Queue', ['path' => '/queue'], function (RouteBuilder $routes) {
+		$routes->connect('/:controller');
+	});
+};
