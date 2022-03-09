bdb2e6ffa015925846d50bffcfc818ee9a5904be
Pascal Schneider <77938819+passchn@users.noreply.github.com>
2022-03-09 23:02:45 +0100

    Don't pass arrays to is_dir() (#321)
    * $filename for is_dir() must be a string. $configuration coult be an array.
    
    * Implode $configuration
    
    * elseif

--- templates/Admin/Queue/index.php
+++ templates/Admin/Queue/index.php
@@ -150,12 +150,14 @@ use Cake\Core\Configure;
 
 			foreach ($configurations as $key => $configuration) {
 				echo '<li>';
-				if (is_dir($configuration)) {
+				if (is_string($configuration) && is_dir($configuration)) {
 					$configuration = str_replace(ROOT . DS, 'ROOT' . DS, $configuration);
 					$configuration = str_replace(DS, '/', $configuration);
 				} elseif (is_bool($configuration)) {
 					$configuration = $configuration ? 'true' : 'false';
-				}
+				} elseif (is_array($configuration)) {
+                    $configuration = implode(', ', $configuration);
+                }
 				echo h($key) . ': ' . h($configuration);
 				echo '</li>';
 			}
