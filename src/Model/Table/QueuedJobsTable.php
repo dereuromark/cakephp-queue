cbfbe0a8653f15d827a31fef0f6848dd0409fcae
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2022-08-22 20:27:47 +0200

    add status column to searchable fields

--- src/Model/Table/QueuedJobsTable.php
+++ src/Model/Table/QueuedJobsTable.php
@@ -135,7 +135,7 @@ class QueuedJobsTable extends Table {
 		$searchManager = $this->behaviors()->Search->searchManager();
 		$searchManager
 			->value('job_task')
-			->like('search', ['fields' => ['job_group', 'reference'], 'before' => true, 'after' => true])
+			->like('search', ['fields' => ['job_group', 'reference', 'status'], 'before' => true, 'after' => true])
 			->add('status', 'Search.Callback', [
 				'callback' => function (Query $query, array $args, $filter) {
 					$status = $args['status'];
