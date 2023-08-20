96ad469b12ad05e7950fefd7bcb6bbefd5295886
Michiel Keijts <michiel@normit.nl>
2023-08-20 15:13:13 +0200

    Add indexes

---
+++ config/Migrations/20230820145600_AddIndexesToQueuedJobs.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddIndexesToQueuedJobs extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function change()
+    {
+		$indexes = [
+		[
+			'reference',
+			'job_task',
+		],[
+			'reference',
+		],[
+			'job_group',
+			'job_task',
+			'notbefore',
+		],[
+			'completed',
+			'failed',
+			'job_group',
+			'job_task',
+			'priority',
+			'notbefore',
+		],[
+			'completed',
+			'job_task',
+			'priority',
+			'notbefore',
+		],[
+			'job_group',
+			'job_task',
+			'notbefore',
+		],[
+			'job_task',
+			'notbefore',
+		]];
+
+		$table = $this->table('queued_jobs');
+		foreach ($indexes as $index) {
+			if ($this->isMigratingUp()) {
+				$table->addIndex($index);
+				continue;
+			}
+			$table->removeIndex($index);
+		}
+
+		$this->isMigratingUp() && $table->update();
+    }
+}
+
+
