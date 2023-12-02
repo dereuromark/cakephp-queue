310bb25930c336ee3b3c4d660f881835d8283fd9
mscherer <dereuromark@web.de>
2023-12-03 00:57:36 +0100

    Add shim

--- config/Migrations/20231112807150_MigrationAddIndex.php
+++ config/Migrations/20231112807150_MigrationAddIndex.php
@@ -8,7 +8,13 @@ class MigrationAddIndex extends AbstractMigration {
 	 * @return void
 	 */
 	public function change() {
-		//FIXME: make sure this is void when a migrating with `202311128071500` instead of `20231112807150` has been run already.
+		// Shim: make sure this is void when a migrating with `202311128071500` instead of `20231112807150` has been run already.
+		$result = $this->query('SELECT * FROM `queue_phinxlog` WHERE version = "202311128071500" LIMIT 1')->fetch();
+		if ($result) {
+			$this->execute('DELETE FROM `queue_phinxlog` WHERE version = "202311128071500" LIMIT 1');
+
+			return;
+		}
 
 		$table = $this->table('queued_jobs');
 		$table
