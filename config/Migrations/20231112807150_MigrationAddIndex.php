4d66cc634c8929d3cce8298fa4cfa6bbd391c140
mscherer <dereuromark@web.de>
2024-03-05 20:52:11 +0100

    Make sure this works for mssql.

--- config/Migrations/20231112807150_MigrationAddIndex.php
+++ config/Migrations/20231112807150_MigrationAddIndex.php
@@ -10,15 +10,21 @@ class MigrationAddIndex extends AbstractMigration {
 	 */
 	public function change() {
 		// Shim: make sure this is void when a migrating with `202311128071500` instead of `20231112807150` has been run already.
-		$result = $this->query('SELECT * FROM queue_phinxlog WHERE version = \'202311128071500\' LIMIT 1')->fetch();
-		if ($result) {
-			$sql = 'DELETE FROM queue_phinxlog WHERE version = \'202311128071500\' LIMIT 1';
-			if (ConnectionManager::getConfig('default')['driver'] === 'Cake\Database\Driver\Sqlserver') {
-				$sql = 'DELETE TOP(1) FROM [queue_phinxlog] WHERE [version] = \'202311128071500\'';
-			}
-			$this->execute($sql);
+		$version = '202311128071500';
+		if (ConnectionManager::getConfig('default')['driver'] === 'Cake\Database\Driver\Sqlserver') {
+			$result = $this->query('SELECT TOP(1) * FROM queue_phinxlog WHERE version = \'' . $version . '\'')->fetch();
+			if ($result) {
+				$this->execute('DELETE TOP(1) FROM queue_phinxlog WHERE version = \'' . $version . '\'');
 
-			return;
+				return;
+			}
+		} else {
+			$result = $this->query('SELECT * FROM queue_phinxlog WHERE version = \'' . $version . '\' LIMIT 1')->fetch();
+			if ($result) {
+				$this->execute('DELETE FROM queue_phinxlog WHERE version = \'' . $version . '\' LIMIT 1');
+
+				return;
+			}
 		}
 
 		$table = $this->table('queued_jobs');
