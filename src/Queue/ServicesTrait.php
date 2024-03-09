77ffc030a138e391474edd0b81073731c89ad705
passchn <77938819+passchn@users.noreply.github.com>
2024-03-09 13:39:44 +0100

    Throw an exception if the container is not set yet (#414)

--- src/Queue/ServicesTrait.php
+++ src/Queue/ServicesTrait.php
@@ -4,25 +4,31 @@ declare(strict_types=1);
 namespace Queue\Queue;
 
 use Cake\Core\ContainerInterface;
+use LogicException;
 
 trait ServicesTrait {
 
 	/**
-	 * @var \Cake\Core\ContainerInterface
+	 * @var \Cake\Core\ContainerInterface|null
 	 */
-	protected ContainerInterface $container;
+	protected ?ContainerInterface $container = null;
 
 	/**
 	 * @template OBJECT
 	 *
 	 * @param class-string<OBJECT> $id Classname or identifier of the service you want to retrieve
 	 *
-	 * @throws \Psr\Container\NotFoundExceptionInterface
 	 * @throws \Psr\Container\ContainerExceptionInterface
+	 * @throws \Psr\Container\NotFoundExceptionInterface
 	 *
 	 * @return OBJECT
 	 */
 	protected function getService(string $id) {
+		if ($this->container === null) {
+			throw new LogicException(
+				"The Container has not been set. Hint: getService() must not be called in the Task's constructor.",
+			);
+		}
 
 		return $this->container->get($id);
 	}
