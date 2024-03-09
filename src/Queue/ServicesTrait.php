16222856a750d8f09fe5cac4d38d9321b9f9e222
Pascal Schneider <77938819+passchn@users.noreply.github.com>
2024-03-09 15:46:07 +0100

    add generic type hint + helpful exception for ServicesTrait (#415)
    * Add generic PHPStan type hint for `ServicesTrait::getService` (#414)
    
    * Throw an exception if the container is not set yet (#414)
    
    * rename type variable (#414)

--- src/Queue/ServicesTrait.php
+++ src/Queue/ServicesTrait.php
@@ -4,23 +4,32 @@ declare(strict_types=1);
 namespace Queue\Queue;
 
 use Cake\Core\ContainerInterface;
+use LogicException;
 
 trait ServicesTrait {
 
 	/**
-	 * @var \Cake\Core\ContainerInterface
+	 * @var \Cake\Core\ContainerInterface|null
 	 */
-	protected ContainerInterface $container;
+	protected ?ContainerInterface $container = null;
 
 	/**
-	 * @param string $id Classname or identifier of the service you want to retrieve
+	 * @template T
 	 *
-	 * @throws \Psr\Container\NotFoundExceptionInterface
+	 * @param class-string<T> $id Classname or identifier of the service you want to retrieve
+	 *
 	 * @throws \Psr\Container\ContainerExceptionInterface
+	 * @throws \Psr\Container\NotFoundExceptionInterface
 	 *
-	 * @return mixed
+	 * @return T
 	 */
-	protected function getService(string $id): mixed {
+	protected function getService(string $id) {
+		if ($this->container === null) {
+			throw new LogicException(
+				"The Container has not been set. Hint: getService() must not be called in the Task's constructor.",
+			);
+		}
+
 		return $this->container->get($id);
 	}
 
