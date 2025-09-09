7d1cd15443971c43a78c40e86e8044220c373016
GustavoPeixoto <gustavopcamargos@gmail.com>
2025-09-09 10:58:11 -0300

    code standards

--- config/Migrations/20250909132048_ChangeQueuedJobsDataLength.php
+++ config/Migrations/20250909132048_ChangeQueuedJobsDataLength.php
@@ -6,52 +6,55 @@ use Phinx\Db\Adapter\AdapterWrapper;
 use Phinx\Db\Adapter\MysqlAdapter;
 use Phinx\Migration\AbstractMigration;
 
-class ChangeQueuedJobsDataLength extends AbstractMigration {
+class ChangeQueuedJobsDataLength extends AbstractMigration
+{
 
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * @return void
-     */
-    public function change() {
-        //TODO: check adapter and skip for postgres, instead of try/catch
-        if ($this->getUnwrappedAdapter() instanceof MysqlAdapter) {
-            try {
-                $table = $this->table('queued_jobs');
-                $table->changeColumn('data', 'text', [
-                    'limit' => MysqlAdapter::TEXT_MEDIUM,
-                    'null' => true,
-                    'default' => null,
-                    'encoding' => 'utf8mb4',
-                    'collation' => 'utf8mb4_unicode_ci',
-                ]);
-                $table->changeColumn('failure_message', 'text', [
-                    'limit' => MysqlAdapter::TEXT_MEDIUM,
-                    'null' => true,
-                    'default' => null,
-                    'encoding' => 'utf8mb4',
-                    'collation' => 'utf8mb4_unicode_ci',
-                ]);
-                $table->update();
-            } catch (Exception $e) {
-                Debugger::dump($e->getMessage());
-            }
-        }
-    }
+	/**
+	 * Change Method.
+	 *
+	 * Write your reversible migrations using this method.
+	 *
+	 * More information on writing migrations is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+	 *
+	 * @return void
+	 */
+	public function change(): void
+	{
+		//TODO: check adapter and skip for postgres, instead of try/catch
+		if ($this->getUnwrappedAdapter() instanceof MysqlAdapter) {
+			try {
+				$table = $this->table('queued_jobs');
+				$table->changeColumn('data', 'text', [
+					'limit' => MysqlAdapter::TEXT_MEDIUM,
+					'null' => true,
+					'default' => null,
+					'encoding' => 'utf8mb4',
+					'collation' => 'utf8mb4_unicode_ci',
+				]);
+				$table->changeColumn('failure_message', 'text', [
+					'limit' => MysqlAdapter::TEXT_MEDIUM,
+					'null' => true,
+					'default' => null,
+					'encoding' => 'utf8mb4',
+					'collation' => 'utf8mb4_unicode_ci',
+				]);
+				$table->update();
+			} catch (Exception $e) {
+				Debugger::dump($e->getMessage());
+			}
+		}
+	}
 
-    private function getUnwrappedAdapter(): ?AdapterInterface
-    {
-        $adapter = $this->adapter;
+	private function getUnwrappedAdapter(): ?AdapterInterface
+	{
+		$adapter = $this->adapter;
 
-        while ($adapter instanceof AdapterWrapper) {
-            $adapter = $adapter->getAdapter();
-        }
+		while ($adapter instanceof AdapterWrapper) {
+			$adapter = $adapter->getAdapter();
+		}
 
-        return $adapter;
-    }
+		return $adapter;
+	}
+
 }
